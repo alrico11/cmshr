7fdfebc3ae32f283ad99d9df6942dea1c283b7db
Alrico Rizki <alricosaiyan123@gmail.com>
2023-07-13 09:32:33 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+export default function IndexLayout() {
+  return (
+    <View style={{marginTop: 40}}>
+      <Text>App</Text>
+    </View>
+  )
+}
